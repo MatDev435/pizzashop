c1f943f86834d67521043bb05459a213b35ca9cf
MatDev435 <matdev435@gmail.com>
2024-01-22 19:13:49 -0300

    Rotas configuradas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from './components/ui/button'
 import './global.css'
 
+import { RouterProvider } from 'react-router-dom'
+
+import { Button } from './components/ui/button'
+import { router } from './routes'
+
 export function App() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Enviar</Button>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
