230b7bfe625109cd91097fa7f478bbe68ca3ce0d
MatDev435 <matdev435@gmail.com>
2024-01-23 17:19:51 -0300

    react-query configurado

---
+++ src/lib/react-query.ts
@@ -0,0 +1,3 @@
+import { QueryClient, Queryclient } from '@tanstack/react-query'
+
+export const queryClient = new QueryClient()
--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,6 +1,6 @@
 import { BarChart } from 'lucide-react'
-import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts'
-import colors, { amber } from 'tailwindcss/colors'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
+import colors from 'tailwindcss/colors'
 
 import {
   Card,
