1b8ff972007ce749acf19bc78815c3a3ae7111bb
MatDev435 <matdev435@gmail.com>
2024-01-24 14:43:20 -0300

    Detalhes do pedido criado

---
+++ src/api/get-order-details.ts
@@ -0,0 +1,33 @@
+import { api } from '@/lib/axios'
+
+export interface GetOrdersDetails {
+  orderId: string
+}
+
+export interface GetOderDetailsResponse {
+  status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+  id: string
+  createdAt: string
+  totalInCents: number
+  customer: {
+    name: string
+    email: string
+    phone: string | null
+  }
+  orderItems: [
+    {
+      id: string
+      priceInCents: number
+      quantity: number
+      product: {
+        name: string
+      }
+    },
+  ]
+}
+
+export async function getOrderDetails({ orderId }: GetOrdersDetails) {
+  const response = await api.get<GetOderDetailsResponse>(`/orders/${orderId}`)
+
+  return response.data
+}
