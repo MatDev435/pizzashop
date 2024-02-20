82fb24bac9de380c3a4d92fdd0c3e53b3ce4c28e
MatDev435 <matheuspsalmazo2@gmail.com>
2024-02-20 14:11:04 -0300

    Configurar os mocks get-profile, update-profile e get-managed-restaurant

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -4,12 +4,15 @@ import { env } from '@/env'
 
 import { GetDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
 import { GetDayOrdersAmountMock } from './get-day-orders-amount'
+import { GetManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { GetMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { GetMonthOrdersAmountMock } from './get-month-orders-amount-mock'
 import { GetMonthRevenueMock } from './get-month-revenue-mock'
 import { GetPopularProductsMock } from './get-popular-products-mock'
+import { GetProfileMock } from './get-profile-mock'
 import { RegisterRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
+import { UpdateProfileMock } from './update-profile-mock'
 
 export const worker = setupWorker(
   signInMock,
@@ -20,6 +23,9 @@ export const worker = setupWorker(
   GetMonthRevenueMock,
   GetDailyRevenueInPeriodMock,
   GetPopularProductsMock,
+  GetProfileMock,
+  GetManagedRestaurantMock,
+  UpdateProfileMock,
 )
 
 export async function enableMSW() {
