30c9f5a1f2ad23db590711d4be1f7f9ba321f5b7
MatDev435 <matheuspsalmazo2@gmail.com>
2024-03-05 13:57:59 -0300

    Criar o teste E2E da página de sign in

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,12 +2,13 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './test',
+  testMatch: /.*\.e2e-spec\.ts$/,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   use: {
-    baseURL: 'http://localhots:50789',
+    baseURL: 'http://localhost:50789',
   },
 
   webServer: {
